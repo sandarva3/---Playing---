93bfbc114271b8ce92ba8e2009d16762afcf8f1e
Sandarva Paudel <sandarvapoudel182@gmail.com>
2024-12-08 09:40:04 +0545

    searching & insertion in binary tree

--- dsaBook/15/binary.py
+++ dsaBook/15/binary.py
@@ -15,3 +15,78 @@ root = Treenode(25, t2, t1)
 
 print(root)
 print(root.view_child())
+
+
+
+#SEARCHING IN BINARY SEARCH TREE using Loop. We pass the root_node as node.
+def search(value, node):
+    while True:
+        if node is None:
+            return f"Value not found."
+        else if node.value == value:
+            return f"Value is in node: {node}"
+        else if:
+            value < node.value:
+                node = node.left_child
+        else:
+            node = node.righ_child
+
+#SEARCHING IN BINARY TREE using Recursion.
+    def search2(value, node):
+    #First base case: if node is None
+        if node is None:
+            return f"Value not found"
+    #Second base case: if we found the value
+        else if node.value == value:
+            return f"Value is in node: {node}"
+        else if:
+            value < node.value:
+                search2(value, node.left_child)
+        else:
+            search2(value, node.right_child)
+
+
+
+
+#INSERTION: Insertion of node using loop.
+def insert(value, node):
+    while True:
+        if value < node.value:
+            if node.left_child is None:
+                new_node = Treenode(value)
+                node.left_child = new_node
+                return f"Inserted as left child in node: {node}"
+            else:
+                node = node.left_child
+        else if value > node.value:
+            if node.right_child is None:
+                new_node = Treenode(value)
+                node.right_child = new_node
+                return f"Inserted as right child in node: {node}"
+            else: node = node.right_child
+        else:
+            return f"Value already exist at the node: {node}"
+
+#INSERTION: Insertion of node using recursion.
+def insert2(value, node):
+    if value < node.value:
+    #In recursion, base case is the case after which occurs then we don't need to make any more recursive call.
+    #Here it's one of the base case.
+        if node.left_child is None:
+            new_node = Treenode(value)
+            node.left_child = new_node
+            return f"Inserted as left child in node: {node}"
+        else:
+            insert2(value, node.left_child)
+    else if value > node.value:
+        if node.right_child is None:
+            new_node = Treenode(value)
+            node.right_child = new_node
+            return f"Inserted as right child in node: {node}"
+        else:
+            insert2(value, node.right_child)
+    else:
+        return f"Value already exist at node: {node}"
+
+
+
